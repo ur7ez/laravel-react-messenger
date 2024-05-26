293053e0defdd1d4b3145403d8f73f41a5356044
ur7ez <ur7ez@ukr.net>
2024-05-26 14:29:47 +0300

    add 2 private channels and define WS event message

--- routes/channels.php
+++ routes/channels.php
@@ -1,8 +1,19 @@
 <?php
 
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('online', function ($user) {
-    return $user ? new UserResource($user) : null;
+// general chat channel
+Broadcast::channel('online', function (User $user) {
+    return new UserResource($user);
+});
+
+// channel for user-to-user messages:
+Broadcast::channel('message.user.{userId1}-{userId2}', function (User $user, int $userId1, int $userId2) {
+    return $user->id === $userId1 || $user->id === $userId2 ? $user : null;
+});
+// channel for group messages:
+Broadcast::channel('message.group.{groupId}', function (User $user, int $groupId) {
+    return $user->groups->contains('id', $groupId) ? $user : null;
 });
