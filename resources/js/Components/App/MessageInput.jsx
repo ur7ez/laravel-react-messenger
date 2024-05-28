e9e8583ee33b12a8f7121b71e801cdb6d1495dfd
ur7ez <ur7ez@ukr.net>
2024-05-28 20:48:54 +0300

    done emoji picker and thumbs up; styled emoji bar with current theme

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -1,6 +1,14 @@
 import {useState} from "react";
-import {PaperClipIcon, PhotoIcon, FaceSmileIcon, HandThumbUpIcon, PaperAirplaneIcon} from "@heroicons/react/24/solid";
+import {
+    PaperClipIcon,
+    PhotoIcon,
+    FaceSmileIcon,
+    HandThumbUpIcon,
+    PaperAirplaneIcon
+} from "@heroicons/react/24/solid";
 import NewMessageInput from "./NewMessageInput";
+import EmojiPicker from 'emoji-picker-react';
+import {Popover, PopoverButton, PopoverPanel, Transition} from '@headlessui/react';
 
 const MessageInput = ({conversation = null}) => {
     const [newMessage, setNewMessage] = useState("");
@@ -26,30 +34,49 @@ const MessageInput = ({conversation = null}) => {
             formData.append("group_id", conversation.id);
         }
 
-        setMessageSending( true);
-        axios.post(route("message.store"), formData, {
-            onUploadProgress: (progressEvent) => {
-                const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
-                console.log(progress);
-            },
-        }).then((res) => {
-            setNewMessage("");
-            setMessageSending(false);
-        }).catch((err) => {
-            setMessageSending(false);
-        });
+        setMessageSending(true);
+        axios
+            .post(route("message.store"), formData, {
+                onUploadProgress: (progressEvent) => {
+                    const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+                    console.log(progress);
+                },
+            })
+            .then((res) => {
+                setNewMessage("");
+                setMessageSending(false);
+            })
+            .catch((err) => {
+                setMessageSending(false);
+            });
+    }
+    const onLikeClick = () => {
+        if (messageSending) {
+            return;
+        }
+        const data = {
+            "message": "👍",
+        };
+        if (conversation.is_user) {
+            data["receiver_id"] = conversation.id;
+        } else if (conversation.is_group) {
+            data["group_id"] = conversation.id;
+        }
+        axios.post(route("message.store"), data);
     }
 
     return (
         <div className="flex flex-wrap items-start border-t border-slate-700 py-3">
             <div className="order-2 flex-1 xs:flex-none xs:order-1 p-2">
                 <button className="p-1 text-gray-400 hover:text-gray-300 relative">
-                    <PaperClipIcon className="w-6" />
-                    <input type="file" multiple className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"/>
+                    <PaperClipIcon className="w-6"/>
+                    <input type="file" multiple
+                           className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"/>
                 </button>
                 <button className="p-1 text-gray-400 hover:text-gray-300 relative">
-                    <PhotoIcon className="w-6" />
-                    <input type="file" multiple accept="image/*" className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"/>
+                    <PhotoIcon className="w-6"/>
+                    <input type="file" multiple accept="image/*"
+                           className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"/>
                 </button>
             </div>
             <div className="order-1 px-3 xs:p-0 min-w-[220px] basis-full xs:basis-0 xs:order-2 flex-1 relative">
@@ -67,7 +94,7 @@ const MessageInput = ({conversation = null}) => {
                         {messageSending && (
                             <span className="loading loading-spinner loading-xs"></span>
                         )}
-                        <PaperAirplaneIcon className="w-6" />
+                        <PaperAirplaneIcon className="w-6"/>
                         <span className="hidden sm:inline">Send</span>
                     </button>
                 </div>
@@ -76,11 +103,20 @@ const MessageInput = ({conversation = null}) => {
                 )}
             </div>
             <div className="order-3 xs:order-3 p-2 flex">
-                <button className="p-1 text-gray-400 hover:text-gray-300">
-                    <FaceSmileIcon className="w-6 h-6" />
-                </button>
-                <button className="p-1 text-gray-400 hover:text-gray-300">
-                    <HandThumbUpIcon className="w-6 h-6" />
+                <Popover className="relative">
+                    <PopoverButton className="p-1 text-gray-400 hover:text-gray-300">
+                        <FaceSmileIcon className="w-6 h-6"/>
+                    </PopoverButton>
+                    <PopoverPanel className="absolute z-10 right-0 bottom-full">
+                        <EmojiPicker
+                            theme="dark"
+                            onEmojiClick={
+                                (ev) => setNewMessage(newMessage + ev.emoji)
+                            }/>
+                    </PopoverPanel>
+                </Popover>
+                <button onClick={onLikeClick} className="p-1 text-gray-400 hover:text-gray-300">
+                    <HandThumbUpIcon className="w-6 h-6"/>
                 </button>
             </div>
         </div>
