a2328a15fa04041e4e112f313cd6e23e2c9e8392
ur7ez <ur7ez@ukr.net>
2024-05-30 23:31:55 +0300

    add message toasts

---
+++ resources/js/Components/App/NewMessageNotification.jsx
@@ -0,0 +1,47 @@
+import {useEventBus} from "@/EventBus";
+import {useEffect, useState} from "react";
+import {v4 as uuidv4} from "uuid";
+import UserAvatar from "@/Components/App/UserAvatar.jsx";
+import {Link} from "@inertiajs/react";
+
+export default function NewMessageNotification({}) {
+    const [toasts, setToasts] = useState([]);
+    const {on} = useEventBus();
+
+    useEffect(() => {
+        on("newMessageNotification", ({message, user, group_id}) => {
+            const uuid = uuidv4();
+
+            setToasts((oldToasts) => [
+                ...oldToasts,
+                {message, uuid, user, group_id}
+            ]);
+            setTimeout(() => {
+                setToasts((oldToasts) => oldToasts.filter((toast) => toast.uuid !== uuid));
+            }, 5000);
+        })
+    }, [on])
+
+    return (
+        <div className="toast toast-top toast-center">
+            {toasts.map((toast) => (
+                <div
+                    key={toast.uuid}
+                    className="alert alert-success py-3 px-4 text-gray-100 rounded-md"
+                >
+                    <Link
+                        className="flex items-center gap-2"
+                        href={
+                            toast.group_id
+                                ? route("chat.group", toast.group_id)
+                                : route("chat.user", toast.user.id)
+                        }
+                    >
+                        <UserAvatar user={toast.user}/>
+                        <span>{toast.message}</span>
+                    </Link>
+                </div>
+            ))}
+        </div>
+    );
+};
